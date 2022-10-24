7eb4436682b2b45a0612cef32d6bc036aeb2b45b
Bilal Mülayim <bilalmulayim@Bilal-MBP.home>
2022-10-24 04:18:42 +0300

    1.08.00 added firebase_config

---
+++ firebase.config.js
@@ -0,0 +1,20 @@
+import {getApp, getApps, initializeApp} from 'firebase/app';
+import {getFirestore} from 'firebase/firestore';
+import {getStorage} from 'firebase/storage'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBIEfJE2TZNiBwgxoxWlTwXlM6jny3oU84",
+    authDomain: "restaurantapp-34a27.firebaseapp.com",
+    databaseURL: "https://restaurantapp-34a27-default-rtdb.firebaseio.com",
+    projectId: "restaurantapp-34a27",
+    storageBucket: "restaurantapp-34a27.appspot.com",
+    messagingSenderId: "681076190914",
+    appId: "1:681076190914:web:2265f77b0afe067a54155b",
+    measurementId: "G-637VVYSC92"
+  };
+
+const app = getApps.Length > 0 ? getApp() : initializeApp(firebaseConfig);
+
+const firestore = getFirestore(app)
+const storage = getStorage(app)
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import Logo from './img/logo.png'
 import Avatar from './img/avatar.png'
 import { MdShoppingBasket } from "react-icons/md";
 import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
 
 
 const Header = () => {
@@ -10,15 +11,15 @@ const Header = () => {
 <header className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16 bg-primary">
   {/* destop and tablet */}
   <div className="hidden md:flex w-full h-full items-center justify-between">
-    <div className='flex items-center gap-2'>
+    <Link to={"/"} className='flex items-center gap-2'>
       <img src={Logo} className="w-8 object-cover" alt="logo"/>
       <p className="text-headingColor text-xl font-bold">ChickHot</p>
-    </div>
+    </Link>
       <ul className="flex items-center gap-8 ml-auto">
-        <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Home</li>
-        <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Menu</li>
-        <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">About Us</li>
-        <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Services</li>
+        <motion.li whileTap={{scale: 0.6}} className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Home</motion.li>
+        <motion.li whileTap={{scale: 0.6}} className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Menu</motion.li>
+        <motion.li whileTap={{scale: 0.6}} className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">About Us</motion.li>
+        <motion.li whileTap={{scale: 0.6}} className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Services</motion.li>
      
     </ul>
           <div
@@ -30,7 +31,12 @@ const Header = () => {
                   2
                 </p>
               </div>
-              <motion.img whileTap={{scale: 0.6}} src={Avatar} className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl ml-3 cursor-pointer" alt="" />
+              <motion.img 
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.8 }}
+              style={{ x: 15 }}
+
+               src={Avatar} className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl ml-3 cursor-pointer" alt="userProfile" />
             
           </div>  
   </div>
