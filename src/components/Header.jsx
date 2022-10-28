b0c403c94c3e787e37867ea06a95d46cc0613699
Bilal Mülayim <bilalmulayim@Bilal-MacBook-Pro.local>
2022-10-28 18:10:26 +0300

    2.15.00

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,6 +18,7 @@ const Header = () => {
   const mobile = "flex md:hidden"; 
   const firebaseAuth = getAuth(app);
   const provider = new GoogleAuthProvider();
+ 
 
   const [{user}, dispatch] = useStateValue();
 
@@ -37,6 +38,16 @@ const Header = () => {
     setIsMenu(!IsMenu);
   }
     }
+
+  const logout = () => {
+    setIsMenu(false)
+    localStorage.clear()
+
+    dispatch({
+      type: actionType.SET_USER,
+      user: null
+    })
+  }
   return (
 <header className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16 bg-primary">
   {/* destop and tablet */}
@@ -45,21 +56,24 @@ const Header = () => {
       <img src={Logo} className="w-8 object-cover" alt="logo"/>
       <p className="text-headingColor text-xl font-bold">ChickHot</p>
     </Link>
-      <ul className="flex items-center gap-8 ml-auto">
-        <motion.li whileTap={{scale: 0.6}} className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Home</motion.li>
-        <motion.li whileTap={{scale: 0.6}} className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Menu</motion.li>
-        <motion.li whileTap={{scale: 0.6}} className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">About Us</motion.li>
-        <motion.li whileTap={{scale: 0.6}} className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Services</motion.li>
+      <motion.ul 
+      initial={{opacity: 0, x:200}}
+      animate={{opacity: 1, x:0}}
+      exit={{opacity: 0, x:200}}
+      className="flex items-center gap-0.5 ml-auto">
+        <motion.li whileTap={{scale: 0.6}} className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2">Home</motion.li>
+        <motion.li whileTap={{scale: 0.6}} className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2">Menu</motion.li>
+        <motion.li whileTap={{scale: 0.6}} className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2">About Us</motion.li>
+        <motion.li whileTap={{scale: 0.6}} className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2">Services</motion.li>
      
-    </ul>
-          <div
-            className="relative flex items-center justify-center">
-            <MdShoppingBasket className="text-textColor text-2xl ml-8 cursor-pointer" />
-            
-              <div className="w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center">
+    </motion.ul>
+              <div className="relative flex items-center justify-center">
+              <MdShoppingBasket className="text-textColor text-2xl  cursor-pointer" />
+                <div className=" absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center">
                 <p className="text-xs text-white font-semibold">
                   2
                 </p>
+                </div>
               </div>
               <div className="relative">
                 <motion.img 
@@ -71,24 +85,75 @@ const Header = () => {
                 />
                 {
                   IsMenu && (
-                    <div className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0">
+                    <motion.div 
+                    initial={{ opacity:0, scale: 0.6 }}
+                    animate={{ opacity:1, scale: 1 }}
+                    exit={{ opacity:0, scale: 0.6 }}
+                     className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0">
                 {user && user.email === "bilalmlym@gmail.com" && (
                   <Link to={'/createItem'}>
                     <p className="flex  items-center gap-3 cursor-pointer hover:bg-slate-200 rounded-lg transition-all duration-100 ease-in-out text text-textColor text-base p-3">New Item  <MdAdd/></p>
                   </Link> )}
-                  <p className=" flex  items-center gap-3 cursor-pointer hover:bg-slate-200 rounded-lg transition-all duration-100 ease-in-out text text-textColor text-base p-3">Logout  <MdLogout/></p>
-                </div>
+                  <p className=" flex  items-center gap-3 cursor-pointer hover:bg-slate-200 rounded-lg transition-all duration-100 ease-in-out text text-textColor text-base p-3"onClick={logout}>Logout  <MdLogout/></p>
+                </motion.div>
                   )
                 }
               </div>
             
           </div>  
-  </div>
+  
 
 
 
   {/* mobile */}
-  <div className={`${mobile} w-screen h-full`}>aslşdşasl</div>
+  <div className={`${mobile} flex items-center justify-between w-full h-full`}>
+  <Link to={"/"} className='flex items-center gap-2'>
+      <img src={Logo} className="w-8 object-cover" alt="logo"/>
+      <p className="text-headingColor text-xl font-bold">ChickHot</p>
+    </Link>
+    <div className="relative flex items-center justify-center">
+            <MdShoppingBasket  className= " text-textColor text-2xl cursor-pointer" />
+              <div className=" absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center">
+                <p className="text-xs text-white font-semibold">
+                  2
+                </p>
+              </div>
+            
+    </div>
+    <div className="relative">
+                <motion.img 
+                whileHover={{ scale: 1.2 }}
+                whileTap={{ scale: 0.8 }}
+                style={{ x: 15 }}
+                src={user ? user.photoURL : Avatar} className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl ml-3 cursor-pointer rounded-full" alt="userProfile"
+                onClick={login}
+                />
+                {
+                  IsMenu && (
+                    <motion.div 
+                    initial={{ opacity:0, scale: 0.6 }}
+                    animate={{ opacity:1, scale: 1 }}
+                    exit={{ opacity:0, scale: 0.6 }}
+                     className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0">
+                {user && user.email === "bilalmlym@gmail.com" && (
+                  <Link to={'/createItem'}>
+                    <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-200 transition-all duration-100 ease-in-out text-textColor text-base">New Item  <MdAdd/></p>
+                  </Link> )}
+                  <ul className="flex  flex-col">
+                    <li  className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-200 px-4 py-2">Home</li>
+                    <li  className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-200 px-4 py-2">Menu</li>
+                    <li  className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-200 px-4 py-2">About Us</li>
+                    <li  className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-200 px-4 py-2">Services</li>
+     
+                  </ul>
+                  <p className=" m-2 p-2 rounded-md shadow-lg flex items-center justify-center bg-black-200 gap-2 cursor-pointer hover:bg-slate-200 rounded-md shadow-md transition-all duration-100 ease-in-out text text-redColor text-base p-3"
+                  onClick={logout}
+                  >Logout<MdLogout/></p>
+                </motion.div>
+                  )
+                }
+              </div>
+  </div>
 </header>
   )
 };
--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const MainContainer = () => {
   return (
-    <div>MainContainer</div>
+    <div className=" grid grid-cols-1 md:grid-cols-2 gap-3 shadow-2xl">
+      <div className="p-4 bg-pink-300 flex flex-col items-start  md:items-center justify-center">
+        <p>Fastest Delivery in Town</p>
+      </div>
+      <div className="p-4 bg-pink-300 flex flex-col items-start md:items-center justify-center"></div>
+    </div>
   )
 }
 
