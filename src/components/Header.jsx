0fbe83f4956893dd00993582ebf31f00e141506e
Bilal Mülayim <bilalmulayim@Bilal-MacBook-Pro.local>
2022-10-29 20:19:48 +0300

    3.18.00

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-import Logo from './img/logo.png'
+import Logo from '../img/logo.png'
 
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { app } from '../firebase.config';
 
 
-import Avatar from './img/avatar.png'
+import Avatar from '../img/avatar.png'
 import { MdShoppingBasket, MdAdd, MdLogout } from "react-icons/md";
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
@@ -49,7 +49,7 @@ const Header = () => {
     })
   }
   return (
-<header className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16 bg-primary">
+<header className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16 bg-primary opacity-95">
   {/* destop and tablet */}
   <div className={`${desktop} w-full h-full items-center justify-between`}>
     <Link to={"/"} className='flex items-center gap-2'>
@@ -92,7 +92,7 @@ const Header = () => {
                      className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0">
                 {user && user.email === "bilalmlym@gmail.com" && (
                   <Link to={'/createItem'}>
-                    <p className="flex  items-center gap-3 cursor-pointer hover:bg-slate-200 rounded-lg transition-all duration-100 ease-in-out text text-textColor text-base p-3">New Item  <MdAdd/></p>
+                    <p className="flex  items-center gap-3 cursor-pointer hover:bg-slate-200 rounded-lg transition-all duration-100 ease-in-out text text-textColor text-base p-3" onClick={() => setIsMenu(false)}>New Item  <MdAdd/></p>
                   </Link> )}
                   <p className=" flex  items-center gap-3 cursor-pointer hover:bg-slate-200 rounded-lg transition-all duration-100 ease-in-out text text-textColor text-base p-3"onClick={logout}>Logout  <MdLogout/></p>
                 </motion.div>
@@ -137,13 +137,13 @@ const Header = () => {
                      className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0">
                 {user && user.email === "bilalmlym@gmail.com" && (
                   <Link to={'/createItem'}>
-                    <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-200 transition-all duration-100 ease-in-out text-textColor text-base">New Item  <MdAdd/></p>
+                    <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-200 transition-all duration-100 ease-in-out text-textColor text-base" onClick={() => setIsMenu(false)}>New Item  <MdAdd/></p>
                   </Link> )}
                   <ul className="flex  flex-col">
-                    <li  className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-200 px-4 py-2">Home</li>
-                    <li  className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-200 px-4 py-2">Menu</li>
-                    <li  className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-200 px-4 py-2">About Us</li>
-                    <li  className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-200 px-4 py-2">Services</li>
+                    <li  className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-200 px-4 py-2"onClick={() => setIsMenu(false)}>Home</li>
+                    <li  className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-200 px-4 py-2"onClick={() => setIsMenu(false)}>Menu</li>
+                    <li  className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-200 px-4 py-2"onClick={() => setIsMenu(false)}>About Us</li>
+                    <li  className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-200 px-4 py-2"onClick={() => setIsMenu(false)}>Services</li>
      
                   </ul>
                   <p className=" m-2 p-2 rounded-md shadow-lg flex items-center justify-center bg-black-200 gap-2 cursor-pointer hover:bg-slate-200 rounded-md shadow-md transition-all duration-100 ease-in-out text text-redColor text-base p-3"
--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -1,27 +1,143 @@
-import React from 'react'
-import Delivery from './img/delivery.png'
+// import React from 'react'
+// import Delivery from '../img/delivery.png'
+// import HeroBg from '../img/heroBg.png'
+// import { heroData } from '../utils/data'
 
-function HomeContainer() {
+
+// function HomeContainer() {
+//   return (
+//     <section className=" grid grid-cols-1 md:grid-cols-2 gap-2 shadow-xl w-full" id='Home'>
+//       <div className="p-4 flex flex-col items-start  md:items-center justify-center gap-6">
+//         <div className="flex items-center justify-center gap-2 bg-orange-200 rounded-full px-3 py-2">
+//         <p className="text-base text-orange-500 font-semibold">Fastest Delivery in Town</p>
+//         <div className="w-9 h-9 bg-slate-50 overflow-hidden rounded-full drop-shadow-xl">
+//         <img src={Delivery} alt="bike-delivery" className="w-full h-full object-contain"/>
+//         </div>
+        
+//       </div>
+//         <p className="text-[2.5rem] font-bold tracking-wide text-headingColor md:text-[4.5rem]">Fastest Delivery in <span className="text-[3rem] md:text-[5rem] text-orange-500">Your City</span></p>
+//         <p className="text-base text-textColor text-center md:text-left ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio cupiditate non ratione repudiandae, voluptatibus ipsam nihil ea incidunt ex! Libero hic at ipsum autem? Error fugit minima recusandae odio sed!</p>
+//         <button type='button' className="bg-gradient-to-br from-orange-400 to-orange-500 w-full md:w-[%80] p-4 rounded-lg hover:shadow-lg ">Order Now</button>
+//         </div>
+        
+//       <div className="py-2 flex items-center relative">
+//         <img src={HeroBg} className="ml-auto h-420 w-full lg:w-auto lg:h-650" alt="HeroBg" />
+//               <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center lg:px-32  py-4 gap-4 flex-wrap">
+//           {heroData &&
+//             heroData.map((n) => (
+//               <div
+//                 key={n.id}
+//                 className="  lg:w-190  p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center drop-shadow-lg"
+//               >
+//                 <img
+//                   src={n.imageSrc}
+//                   className="w-20 lg:w-40 -mt-10 lg:-mt-20 "
+//                   alt="I1"
+//                 />
+//                 <p className="text-base lg:text-xl font-semibold text-textColor mt-2 lg:mt-4">
+//                   {n.name}
+//                 </p>
+
+//                 <p className="text-[12px] lg:text-sm text-lighttextGray font-semibold my-1 lg:my-3">
+//                   {n.decp}
+//                 </p>
+
+//                 <p className="text-sm font-semibold text-headingColor">
+//                   <span className="text-xs text-red-600">$</span> {n.price}
+//                 </p>
+//               </div>
+//             ))}
+        
+//         </div>  
+//       </div>
+//     </section>
+//   )
+// }
+
+// export default HomeContainer
+
+import React from "react";
+import Delivery from "../img/delivery.png";
+import HeroBg from "../img/heroBg.png";
+import { heroData } from "../utils/data";
+
+const HomeContainer = () => {
   return (
-    <div className=" grid grid-cols-1 md:grid-cols-2 gap-3 shadow-2xl">
-      <div className="p-4 flex flex-col items-start  md:items-center justify-center gap-6">
-        <div className="flex items-center justify-center gap-2 bg-orange-200 rounded-full px-3 py-2">
-        <p className="text-base text-orange-500 font-semibold">Fastest Delivery in Town</p>
-        <div className="w-9 h-9 bg-slate-50 overflow-hidden rounded-full drop-shadow-xl">
-        <img src={Delivery} alt="bike-delivery" className="w-full h-full object-contain"/>
+    <section
+      className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full "
+      id="home"
+    >
+      <div className="py-2 flex-1 flex flex-col items-start justify-center gap-6">
+        <div className="flex items-center gap-2 justify-center bg-orange-100 px-4 py-1 rounded-full">
+          <p className="text-base text-orange-500 font-semibold">
+            Bike Delivery
+          </p>
+          <div className="w-8 h-8 bg-white rounded-full overflow-hidden drop-shadow-xl">
+            <img
+              src={Delivery}
+              className="w-full h-full object-contain"
+              alt="delivery"
+            />
+          </div>
         </div>
-        
-      </div>
-        <p className="text-[2.5rem] font-bold tracking-wide text-headingColor md:text-[4.5rem]">Fastest Delivery in <span className="text-[3rem] md:text-[5rem] text-orange-500">Your City</span></p>
-        <p className="text-base text-textColor text-center md:text-left ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio cupiditate non ratione repudiandae, voluptatibus ipsam nihil ea incidunt ex! Libero hic at ipsum autem? Error fugit minima recusandae odio sed!</p>
-        <button type='button' className="bg-gradient-to-br from-orange-400 to-orange-500 w-full md:w-[%80] p-4 rounded-lg hover:shadow-lg ">Order Now</button>
-        </div>
-        
-      <div className="p-4 bg-pink-50 flex flex-col items-start md:items-center justify-center">
-        <p className="text-base text-orange-500 font-sans">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium nemo assumenda praesentium explicabo, reiciendis quas tenetur maiores, debitis est vel corporis odio ducimus. Quis eaque maxime cum reiciendis exercitationem harum.</p>
-      </div>
-    </div>
-  )
-}
 
-export default HomeContainer
+        <p className="text-[2.5rem] lg:text-[4.5rem] font-bold tracking-wide text-headingColor">
+          The Fastest Delivery in
+          <span className="text-orange-600 text-[3rem] lg:text-[5rem]">
+            Your City
+          </span>
+        </p>
+
+        <p className="text-base text-textColor text-center md:text-left md:w-[80%]">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima velit
+          eaque fugit distinctio est nam voluptatum architecto, porro iusto
+          deserunt recusandae ipsa minus eos sunt, dolores illo repellat facere
+          suscipit!
+        </p>
+
+        <button
+          type="button"
+          className="bg-gradient-to-br from-orange-400 to-orange-500 w-full md:w-auto px-4 py-2  rounded-lg hover:shadow-lg transition-all ease-in-out duration-100"
+        >
+          Order Now
+        </button>
+      </div>
+      <div className="py-2 flex-1 flex items-center relative">
+        <img
+          src={HeroBg}
+          className=" ml-auto h-420 w-full lg:w-auto lg:h-650"
+          alt="hero-bg"
+        />
+
+        <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center lg:px-32  py-4 gap-4 flex-wrap">
+          {heroData &&
+            heroData.map((n) => (
+              <div
+                key={n.id}
+                className="  lg:w-190  p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center drop-shadow-lg"
+              >
+                <img
+                  src={n.imageSrc}
+                  className="w-20 lg:w-40 -mt-10 lg:-mt-20 "
+                  alt="I1"
+                />
+                <p className="text-base lg:text-xl font-semibold text-textColor mt-2 lg:mt-4">
+                  {n.name}
+                </p>
+
+                <p className="text-[12px] lg:text-sm text-lighttextGray font-semibold my-1 lg:my-3">
+                  {n.decp}
+                </p>
+
+                <p className="text-sm font-semibold text-headingColor">
+                  <span className="text-xs text-red-600">$</span> {n.price}
+                </p>
+              </div>
+            ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HomeContainer;
