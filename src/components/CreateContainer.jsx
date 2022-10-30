487d72e58381e5a4748f8ef15a657ed3a295ab9c
Bilal Mülayim <bilalmulayim@Bilal-MBP.home>
2022-10-30 05:21:15 +0300

    3.31.00

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,34 +1,42 @@
-import { useState } from "react";
-import React from 'react'
+import React,{ useState } from "react";
+import { motion } from "framer-motion";
+import {MdFastfood} from "react-icons/md";
 
+const CreateContainer = () => {
 
-  const [title, setTitle] = useState("");
-  const [calories, setCalories] = useState("");
-  const [price, setPrice] = useState("");
+  const [title, setTitle] = useState(null);
+  const [calories, setCalories] = useState(null);
+  const [price, setPrice] = useState(null);
   const [category, setCategory] = useState(null);
   const [imageAsset, setImageAsset] = useState(null);
-  const [fields, setFields] = useState(true);
+  const [fields, setFields] = useState(false);
   const [alertStatus, setAlertStatus] = useState("danger");
   const [msg, setMsg] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-
-const CreateContainer = () => {
 
 
   return (
     <div className="w-full min-h-screen flex items-center justify-center">
       <div className="w-[90%] md:w-[75%] border border-grey-500 rounded-lg p-4 flex flex-col items-center justify-center">
-        { fields && (
-          <p className={"w-full p-2 rounded-lg text-center ${
+        {fields && (
+          <motion.p 
+          initial={{opacity:0}}
+          animate={{opacity:1}}
+          exit={{opacity:0}}
+          className={`w-full p-2 rounded-lg text-center text-lg font-semibold ${
             alertStatus === "danger"
-            ? "bg-red-500 text-red-500"
-            : "bg-green-500 text-green-400"
-          }"}
-          >
-            Something wrong
-          </p>
-        )
-        }
+              ? "bg-red-400 text-red-800"
+              : "bg-emerald-400 text-emerald-800"
+          }`}>
+            {msg}
+          </motion.p>
+          )}
+          <div className="w-full py-2 border border-gray-500 flex items-center">
+            <MdFastfood className="m-2 text-xl text-gray-700"/>
+            <input type="text" required value={title} 
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder='Give me a title' className="w-full h-full text-lg bg-transparent outline-none border-none placeholder:text-gray-400 text-textColor" />
+          </div>
       </div>
     </div>
   )
