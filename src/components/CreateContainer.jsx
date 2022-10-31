dfea2e8205f141c5dd6baa2849c0a05d48f63963
Bilal Mülayim <bilalmulayim@Bilal-MBP.home>
2022-10-31 23:29:53 +0300

    3.44.00

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,6 +1,12 @@
 import React,{ useState } from "react";
 import { motion } from "framer-motion";
-import {MdFastfood} from "react-icons/md";
+import { MdFastfood,
+  MdCloudUpload,
+  MdDelete,
+  MdFoodBank,
+  MdAttachMoney} from "react-icons/md";
+import { categories } from "../utils/data";
+import Loader from "./Loader";
 
 const CreateContainer = () => {
 
@@ -17,7 +23,7 @@ const CreateContainer = () => {
 
   return (
     <div className="w-full min-h-screen flex items-center justify-center">
-      <div className="w-[90%] md:w-[75%] border border-grey-500 rounded-lg p-4 flex flex-col items-center justify-center">
+      <div className="w-[90%] md:w-[75%] border border-grey-500 rounded-lg p-4 flex flex-col items-center justify-center gap-4">
         {fields && (
           <motion.p 
           initial={{opacity:0}}
@@ -37,7 +43,45 @@ const CreateContainer = () => {
             onChange={(e) => setTitle(e.target.value)}
             placeholder='Give me a title' className="w-full h-full text-lg bg-transparent outline-none border-none placeholder:text-gray-400 text-textColor" />
           </div>
-      </div>
+          <div className="w-full">
+            <select onChange={(e) => setCategory(e.target.value)} className="outline-none w-full text-base border-b-2 border-grey-200 p-2 border-rounded cursor-pointer " >
+              <option value="other" className="bg-white">Select Category</option>
+              {categories && categories.map((item) => (
+                <option key={item.id} className=" text-base border-0 outline-none capitalize text-headingColor bg-white" value={item.urlParamName} >
+                  {item.name}
+                </option>
+              ))}             
+            </select>
+          </div>
+          <div className="group flex justify-center items-center flex-col border-2 border-dotted border-gray-300 w-full h-225 md:h-340 cursor-pointer rounded-lg">
+          {isLoading ? (
+            <Loader />
+          ) : (
+            <>
+              {!imageAsset ? (
+                <>
+                  <label className="w-full h-full flex flex-col items-center justify-center cursor-pointer">
+                    <div className="w-full h-full flex flex-col items-center justify-center gap-2">
+                      <MdCloudUpload className="text-gray-500 text-3xl hover:text-gray-700" />
+                      <p className="text-gray-500 hover:text-gray-700">
+                        Click here to upload
+                      </p>
+                    </div>
+                    
+                  </label>
+                </>
+              ) : (
+                <>
+                  <div className="relative h-full">
+                    
+                   
+                  </div>
+                </>
+              )}
+            </>
+          )}
+        </div>      
+        </div>
     </div>
   )
 }
