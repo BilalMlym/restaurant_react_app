394bfd433a0201969b6c17ddda065b5525c71c53
Bilal Mülayim <bilalmulayim@Bilal-MBP.home>
2022-11-02 19:51:34 +0300

    2 error in create window

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -7,6 +7,14 @@ import { MdFastfood,
   MdAttachMoney} from "react-icons/md";
 import { categories } from "../utils/data";
 import Loader from "./Loader";
+import {
+  deleteObject,
+  getDownloadURL,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { storage } from "../firebase.config";
+
 
 const CreateContainer = () => {
 
@@ -19,6 +27,15 @@ const CreateContainer = () => {
   const [alertStatus, setAlertStatus] = useState("danger");
   const [msg, setMsg] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+
+  const uploadImage=(e)=>{
+    setIsLoading(true);
+    const imageFile = e.target.files[0];
+    const  storageRef = ref(storage, 'Images/${Date.now()}-${imageFile.name}');
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+
+  const deleteImage=()=>{}
+  const saveDetails=()=>{}
 
 
   return (
@@ -53,7 +70,7 @@ const CreateContainer = () => {
               ))}             
             </select>
           </div>
-          <div className="group flex justify-center items-center flex-col border-2 border-dotted border-gray-300 w-full h-225 md:h-340 cursor-pointer rounded-lg">
+        <div className="group flex justify-center items-center flex-col border-2 border-dotted border-gray-300 w-full h-225 md:h-340 cursor-pointer rounded-lg">
           {isLoading ? (
             <Loader />
           ) : (
@@ -67,23 +84,39 @@ const CreateContainer = () => {
                         Click here to upload
                       </p>
                     </div>
+                    <input type="file" name="uploadimage" accept="image/*" onChange={uploadImage} className="w-0 h-0" />
                     
                   </label>
                 </>
               ) : (
                 <>
                   <div className="relative h-full">
-                    
-                   
+                    <img src={imageAsset} alt="uploadedimage" className="w-full h-full object-cover "/>
+                    <button type="button" className="absolute  bottom-3 right-3 p-3 rounded-full bg-pink-500 text-xl cursor-pointer outline-none hover:shadow-md duration-500 transition-all ease-in-out " onClick={deleteImage}>
+                      <MdDelete className="bg-red-500"/>
+                    </button>
                   </div>
                 </>
               )}
             </>
           )}
-        </div>      
+        </div>
+          <div className="w-full flex items-center flex-col md:flex-row gap-3" >
+            <div className="w-full py-2 border-b border-gray-300 flex items-center gap-2">
+                <MdFoodBank className="text-gray-700 text-2xl" />
+                <input type="text" required  value={calories} onChange={(e) => setCalories(e.target.value)} placeholder="Calories" className="w-full h-full text-lg bg-transparent outline-none border-none placeholder:text-textColor " />
+            </div>
+            <div className="w-full py-2 border-b border-gray-300 flex items-center gap-2">
+                <MdAttachMoney className="text-gray-700 text-2xl" />
+                <input type="text" required value={price} onChange={(e) => setPrice(e.target.price)} placeholder="Price" className="w-full h-full text-lg bg-transparent outline-none border-none placeholder:text-textColor " />
+            </div>
+          </div>
+          <div className="flex items-center w-full">
+            <button type="button" className="ml-0 md:ml-auto w-full md:w-auto border-none outline-none bg-emerald-500 px-12 py-2 rounded-lg text-lg text-white font-semibold" onClick={saveDetails}>Save</button>
+          </div>      
         </div>
     </div>
   )
-}
+};
 
-export default CreateContainer
+export default CreateContainer;
