a67771e007d4045e02f5c15e37e0c68c155ea3ef
Bilal Mülayim <bilalmulayim@Bilal-MBP.home>
2022-11-03 18:16:02 +0300

    4.36.40

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -5,6 +5,11 @@ const MainContainer = () => {
   return (
     <div className="w-full h-auto flex flex-col items-center justify-center ">
      <HomeContainer/>
+     <section className="w-full">
+        <div className=" w-full flex items-center justify-between">
+          <p className="text-lg font-semibold capitalize text-headingColor relative">Our Fresh & Healthy Fruits</p>
+        </div>
+     </section>
     </div>
   )
 }
--- src/context/initialState.js
+++ src/context/initialState.js
@@ -6,5 +6,6 @@ const userInfo = fetchUser();
 
 export const initialState = {
   user: userInfo,
+  foodItems : null
   
 };
